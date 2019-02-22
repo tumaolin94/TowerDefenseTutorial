f3581058e0a5cc6ee0273b73e68adc5f2bc8c1d4
Maolin Tu <tumaolin94@gmail.com>
2019-02-21 16:03:32 -0800

    shooting

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -6,13 +6,21 @@ public class Turret : MonoBehaviour
 {
 
     public Transform target;
-    public float range = 15f;
 
+    [Header("Attributes")]
+    public float range = 15f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -60,8 +68,29 @@ public class Turret : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
 
     }
+
+    void Shoot()
+    {
+        Debug.Log(transform.name + "  SHOOT!");
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+
 
     private void OnDrawGizmosSelected()
     {
