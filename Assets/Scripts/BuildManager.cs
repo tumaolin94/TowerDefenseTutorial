a71c909318804bd3a5d04258d7cf98bbf67c0b9c
Maolin Tu <tumaolin94@gmail.com>
2019-02-23 14:47:10 -0800

    add shop

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -16,16 +16,22 @@ public class BuildManager : MonoBehaviour
     }
 
     public GameObject standardTurretPrefab;
+    public GameObject anotherTurretPrefab;
 
-    void Start()
-    {
-        turretToBuild = standardTurretPrefab;
-    }
+    //void Start()
+    //{
+    //    turretToBuild = standardTurretPrefab;
+    //}
 
     private GameObject turretToBuild;
 
     public GameObject GetTurretToBuild()
     {
         return turretToBuild;
+    }
+
+    public void SetTurretToBuild(GameObject turret)
+    {
+        turretToBuild = turret;
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
@@ -10,15 +11,30 @@ public class Node : MonoBehaviour
     private Renderer rend;
     private Color startColor;
 
+    BuildManager buildManager;
+
+
     void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
     }
 
     void OnMouseDown()
     {
-        if(turret != null)
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
+
+        if (turret != null)
         {
             Debug.Log("Can't build there! - TODO: Display on Screen.");
             return;
@@ -26,16 +42,31 @@ public class Node : MonoBehaviour
 
         //Build a turret
         GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+
+
+
         turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+
+
     }
 
     void OnMouseEnter()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
         rend.material.color = hoverColor;
     }
 
     void OnMouseExit()
     {
+
         rend.material.color = startColor;
     }
 }
